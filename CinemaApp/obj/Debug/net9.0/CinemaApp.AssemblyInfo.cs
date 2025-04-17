b813c70a6f925218481574188ccddee6900feeba
vittoriodedi <151766757+vittoriodedi@users.noreply.github.com>
2025-04-17 23:33:30 +0200

    terzo commit

--- CinemaApp/obj/Debug/net9.0/CinemaApp.AssemblyInfo.cs
+++ CinemaApp/obj/Debug/net9.0/CinemaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b549570ac0dda26411545b77a702ba89dbbf72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+078ae3578c14cbcd141a7a6bc5200cd0cafe44c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
