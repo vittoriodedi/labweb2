89470940456f53e7c9ae98bbe7b462a673719e99
vittoriodedi <151766757+vittoriodedi@users.noreply.github.com>
2025-05-01 16:02:15 +0200

    Settimo Commit - Modifiche alle pagine Biglietti, About e Contatti (per renderle graficamente coerenti)

--- CinemaApp/CinemaApp/obj/Debug/net9.0/CinemaApp.AssemblyInfo.cs
+++ CinemaApp/CinemaApp/obj/Debug/net9.0/CinemaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca15945c02e625c2caad20866425f2419315d41c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea787a501a9b8ee25a12e1311bc3a46859011df")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
