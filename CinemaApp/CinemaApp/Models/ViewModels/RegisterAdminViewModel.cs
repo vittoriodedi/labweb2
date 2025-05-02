8a606a86c41ce6e9f08107d3e6a4f7738f80e23f
vittoriodedi <151766757+vittoriodedi@users.noreply.github.com>
2025-05-02 22:01:06 +0200

    Nono Commit - Modifica della pagina di registrazione degli Admin

---
+++ CinemaApp/CinemaApp/Models/ViewModels/RegisterAdminViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApp.Models.ViewModels
+{
+    public class RegisterAdminViewModel
+    {
+        [Required(ErrorMessage = "L'email è richiesta")]
+        [EmailAddress(ErrorMessage = "Formato email non valido")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Il nome utente è richiesto")]
+        [MinLength(3, ErrorMessage = "Il nome utente deve avere almeno 3 caratteri")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La password è richiesta")]
+        [MinLength(6, ErrorMessage = "La password deve avere almeno 6 caratteri")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La conferma della password è richiesta")]
+        [Compare("Password", ErrorMessage = "Le password non corrispondono")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
--- CinemaApp/CinemaApp/obj/Debug/net9.0/CinemaApp.AssemblyInfo.cs
+++ CinemaApp/CinemaApp/obj/Debug/net9.0/CinemaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89470940456f53e7c9ae98bbe7b462a673719e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57c3689bb62a136833bcacd071bae2b75b52cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
